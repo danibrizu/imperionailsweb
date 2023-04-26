c43d43c28fd0da77897724001ffc7bb603e3ddce
danibrizu <danitobrizu@gmail.com>
2023-04-26 13:41:36 -0300

    Primera subida

---
+++ js/index.js
@@ -0,0 +1,34 @@
+// let carousel = document.getElementById("carouselExampleAutoplaying");
+// // let container = carousel.parentElement;
+// let container = document.getElementById("container");
+
+// carousel.addEventListener("click", () => {
+//     container.style.height = carousel.offsetHeight + 'px';
+//     console.log(carousel);
+//     console.log(container);
+//     console.log(`El alto de ${carousel.id} es ${carousel.offsetHeight}`);
+//     console.log(`El alto de ${container.id} es ${container.style.height}`);
+// })
+
+// Leer un archivo HTML para el Menu
+// let htmlFileMenu = new XMLHttpRequest();
+// let filePathMenu = "../menu.html";
+// htmlFileMenu.open("GET", filePathMenu, false);
+// htmlFileMenu.send(null);
+
+// console.log(htmlFile.responseText);
+
+// let menu = document.querySelector(".header");
+// menu.innerHTML = htmlFileMenu.responseText
+
+
+// Leer un archivo HTML para el Footer
+// let htmlFileFooter = new XMLHttpRequest();
+// let filePathFooter = "../footer.html";
+// htmlFileFooter.open("GET", filePathFooter, false);
+// htmlFileFooter.send(null);
+
+// // console.log(htmlFile.responseText);
+
+// let footer = document.querySelector(".footer");
+// footer.innerHTML = htmlFileFooter.responseText;
---
+++ js/menu.js
@@ -0,0 +1,72 @@
+// function readFile (file) {
+//     const reader = new FileReader();
+//     reader.addEventListener('loadend', (event) => {
+//         img.src = event.target.result;
+//         const result = event.target.result;
+//         console.log(result);
+//     });
+//     console.log(file);
+//     console.log(self.location.host + file);
+//     reader.readAsText(self.location.host + file);
+// }
+
+// var reader = new FileReader();
+// var fileToRead = self.location.href; //document.querySelector('input').files[0];
+// let posicionUltimaBarraMenu = fileToRead.lastIndexOf("/");
+// let rutaRelativaMenu = fileToRead.substring(0, posicionUltimaBarraMenu + 1 );
+
+// // attach event, that will be fired, when read is end
+// reader.addEventListener("loadend", function() {
+//    // reader.result contains the contents of blob as a typed array
+//    // we insert content of file in DOM here
+//    document.getElementById('header').innerText = reader.result;
+// });
+
+// // start reading a loaded file
+// console.log(`${rutaRelativaMenu}menu.txt`);
+// reader.readAsText(URL.createObjectURL(`${rutaRelativaMenu}menu.txt`));
+
+// const menuFile = readFile("/menu.txt");
+// let menu = document.querySelector(".header");
+// menu.innerHTML = menuFile //htmlFileMenu.responseText
+
+// ******************************************************************************************
+// let fs = require('fs');
+// let buffer = fs.readFileSync("../menu.txt");
+// let texto = buffer.toString();
+// console.log(texto);
+
+// *********************************************************************************************
+// Obtengo el nombre del archivo html
+let rutaAbsoluta = self.location.href;
+let posicionUltimaBarra = rutaAbsoluta.lastIndexOf("/");
+let rutaRelativa = rutaAbsoluta.substring( posicionUltimaBarra + "/".length , rutaAbsoluta.length );
+// console.log(rutaRelativa);
+
+// Selecciono los elmento del menu y le quito la clase focus
+let navLinkClick = document.querySelectorAll(".nav-link");
+for (i = 0; i < navLinkClick.length; i++) {
+    navLinkClick[i].classList.remove("focus");
+}
+
+// Asigno la clase focus al menu correspondiente en función al archivo html que se esta mostrando
+switch (rutaRelativa) {
+    case "index.html":
+        navLinkClick[0].classList.add("focus");
+        break;
+    case "nosotros.html":
+        navLinkClick[1].classList.add("focus");
+        break;
+    case "galeria.html":
+        navLinkClick[2].classList.add("focus");
+        break;
+    case "blog.html":
+        navLinkClick[3].classList.add("focus");
+        break;
+    case "carrito.html":
+        navLinkClick[4].classList.add("focus");
+        break;
+    case "contacto.html":
+        navLinkClick[5].classList.add("focus");
+    break;
+}
